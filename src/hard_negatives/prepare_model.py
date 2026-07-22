90bc2f55d04022032fae40a32791ccb0a6932e5c
Enrico Shippole <enricoship@gmail.com>
2026-07-22 13:33:03 -0400

    remove flash attention 2 for the time being

--- src/hard_negatives/prepare_model.py
+++ src/hard_negatives/prepare_model.py
@@ -6,9 +6,10 @@ def load_model(model_name: str, max_seq_len: int):
     model = SentenceTransformer(
         model_name,
         model_kwargs={
-            "attn_implementation": "flash_attention_2",
+            "attn_implementation": "sdpa",
             "dtype": torch.bfloat16,
         },
     )
     model.max_seq_length = max_seq_len
+    model = torch.compile(model)
     return model
